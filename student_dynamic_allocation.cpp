9656e3a9f1748c446dbaeac455cebdfe6c39b9e1
jayathrikanapuram <jayathrikanapuram@gmail.com>
2026-02-07 14:50:42 +0530

    student_dynamic_allocation.cpp

--- student_dynamic_allocation.cpp
+++ student_dynamic_allocation.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main () {
-    cout <<"Hello World" <<endl;
-    return 0;
+class Student{
+private:
+int rollNumber;
+string name;
+float marks;
+public;
+Student(){
+rollNumber = 0;
+name = " ";
+marks = 0.00;
+}
+void input(){
+    cout << "Enter Roll Number : ";
+cin >> rollNumber;
+cin.ignore();
+cout<<"Enter name : ";
+getline(cin, name);
+cout << "Enter marks : ";
+cin >> "marks;
+    }
+void display(){
+    cout << rollNumber << "\t" << name << "\t" << marks<< endl;
+}
+~Student(){
+cout<< " Student object with roll number "<<rollNumber<<"is destroyed";
+}
+};
+int main(){
+    int n;
+cout<<"Enter no.of students";
+cin>>n;
+Student *Students = newstudent[n];
+for(int i = 0; i < n; i++){
+cout<<"\n Enter details of student " << i+1<<endl;
+Students[i].input();
+}
+cout<<"\nRollNo\tName\tMarks\n";
+for(int i = 0; i < n; i++){
+student[i].display();
+}
+delete[] Students;
+return 0;
 }
